7fdfa860e189a933a209b3265243ca3135485c06
LightningMcQueen <e232177@sbl.ch>
2022-11-17 12:00:35 +0100

    finished awsome game

---
+++ script.js
@@ -0,0 +1,8 @@
+function handleKeyDown(ev) {
+const player = document.querySelector("#player")
+player.setAttribute("cx", 100 * Math.random())
+player.setAttribute("cy", 100 * Math.random())
+}
+
+
+window.onkeydown = handleKeyDown
